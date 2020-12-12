dea2e0150af0f4e31c609862bd43095408d6f7cb
Jaon-li <17717561090li>
2020-12-12 21:57:31 +0800

    配置feign

--- src/main/java/com/blog/bloguser/controller/UserController.java
+++ src/main/java/com/blog/bloguser/controller/UserController.java
@@ -1,5 +1,58 @@
 package com.blog.bloguser.controller;
 
-public class
-UserController implements UserApi {
+import com.blog.base.userApi.api.UserApi;
+import com.blog.base.userApi.request.LoginEntity;
+import com.blog.base.userApi.request.RegisterEntity;
+import com.blog.base.userApi.request.RestPwdEntity;
+import com.blog.base.userApi.request.UserInfoEntity;
+import com.blog.base.userApi.response.BaseResponse;
+import com.blog.base.userApi.response.UserInfo;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController implements UserApi {
+    @Override
+    public BaseResponse<UserInfo> register(RegisterEntity registerEntity) {
+        return null;
+    }
+
+    @Override
+    public BaseResponse<String> imgCode(String s) {
+        return null;
+    }
+
+    @Override
+    public BaseResponse<String> emailCode(String s) {
+        return null;
+    }
+
+    @Override
+    public BaseResponse<UserInfo> login(LoginEntity loginEntity) {
+        return null;
+    }
+
+    @Override
+    public BaseResponse<UserInfo> getUserInfo(String s) {
+        return null;
+    }
+
+    @Override
+    public BaseResponse<Boolean> updateUserInfo(UserInfoEntity userInfoEntity) {
+        return null;
+    }
+
+    @Override
+    public BaseResponse<Boolean> resetPwd(RestPwdEntity restPwdEntity) {
+        return null;
+    }
+
+    @Override
+    public BaseResponse<Boolean> Logout(String s) {
+        return null;
+    }
+
+    @Override
+    public BaseResponse<Boolean> DestroyUser(String s) {
+        return null;
+    }
 }
