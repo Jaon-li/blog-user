accce900a17e6e4de5c932ae65de2c7ae8b72547
Jaon-li <17717561090li>
2021-01-24 16:10:30 +0800

    1.1

--- src/main/java/com/blog/bloguser/BlogUserApplication.java
+++ src/main/java/com/blog/bloguser/BlogUserApplication.java
@@ -1,13 +1,17 @@
 package com.blog.bloguser;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @EnableFeignClients
+@EnableScheduling
+@MapperScan("com.blog.bloguser.mapper")
 public class BlogUserApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/blog/bloguser/domain/User.java
@@ -0,0 +1,23 @@
+package com.blog.bloguser.domain;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@TableName("user")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+//    @TableId(value = "id", type = IdType.AUTO)
+    private Integer id;// int          null,
+    private String account;// varchar(255) null,
+    private String password;// varchar(255) null,
+    private String nick;// varchar(255) null,
+    private String phone;// varchar(255) null
+
+}
